c2495e63099e2cf37fdf51a94ca25e8af0c119e9
Maria Rykova <maria.nigay@yandex.ru>
2021-04-11 18:18:52 +0500

    test

--- scripts.js
+++ scripts.js
@@ -1,14 +1,30 @@
+const requestURL = "https://603e38c548171b0017b2ecf7.mockapi.io/homes";
+
 let xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.state == 200) {
-        myFunction(this.responseText);
-    }
+xhttp.open('GET', requestURL);
+
+xhttp.onload = () => {
+    console.log(xhttp.response);
 }
 
-xhttp.open("GET", "https://603e38c548171b0017b2ecf7.mockapi.io/homes", true);
 xhttp.send();
 
-function myFunction(data) {
-    console.log(data);
-}
+
+// xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.state == 200) {
+//         myFunction(this.responseText)
+//     }
+// }
+
+
+// xhttp.responseType = 'json';
+
+
+
+// function myFunction(data) {
+//     console.log(data);
+//     console.log('hello');
+// }
+
+// console.log('hello');
